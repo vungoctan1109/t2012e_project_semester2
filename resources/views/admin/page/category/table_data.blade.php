68c452a9a3689fde79e748b4b5159ec3892cfb87
vungoctan1109 <vntan1109@gmail.com>
2021-11-25 17:29:02 +0700

    edit category completed

--- resources/views/admin/page/category/table_data.blade.php
+++ resources/views/admin/page/category/table_data.blade.php
@@ -5,12 +5,12 @@
 @section('page-title','Admin | Table')
 @section('breadcrumb')
 <div class="col-sm-6">
-    <h1 class="m-0">Product</h1>
+    <h1 class="m-0">Category</h1>
 </div><!-- /.col -->
 <div class="col-sm-6">
     <ol class="breadcrumb float-sm-right">
         <li class="breadcrumb-item"><a href="#">Home</a></li>
-        <li class="breadcrumb-item active"> Table</li>
+        <li class="breadcrumb-item active">Category / List </li>
     </ol>
 </div><!-- /.col -->
 @endSection
