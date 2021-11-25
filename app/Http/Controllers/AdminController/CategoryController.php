c0e8a06ca930461499f5dbd005924cf129abfeaf
vungoctan1109 <vntan1109@gmail.com>
2021-11-25 18:05:04 +0700

    fix update

--- app/Http/Controllers/AdminController/CategoryController.php
+++ app/Http/Controllers/AdminController/CategoryController.php
@@ -137,6 +137,7 @@ class CategoryController extends Controller
             return response() -> json(['status' => 400, 'errors' => $validator -> errors() -> toArray(),'message' => 'Data not valid!']);
         } else {
             $result = DB::table('categories')->where('id', '=', $id)->update($data);
+            Category::where('id', $id)->update(array('updated_at'=>Carbon::now()));
             if ($result) {
                 return response()->json(['status' => 200, 'message' => 'Data have been successfully update']);
             }
