41a32f57dfcd07ebe43ab3568fa9ffb55b10d30b
vungoctan1109 <vntan1109@gmail.com>
2021-11-27 20:48:45 +0700

    update seeder mobile

--- public/dist/js/pages/mobile/create_mobile.js
+++ public/dist/js/pages/mobile/create_mobile.js
@@ -24,9 +24,9 @@ $(document).ready(function () {
                 // alert(document.getElementById("valueUpLoad").value);
                 document.getElementById("list-preview-image").innerHTML += `
                <span class="m-2" id="preview-image" style="position: relative; with:220px; display:inline-block;">
-                   <img src="${result.info.secure_url}" class="img-thumbnail img-bordered" style="width: 220px; ml-2" delete="${result.info.delete_token}">                    
-                   <i class="fas fa-times btnDeleteImg" style="position: absolute;right: 0;top: 0; cursor: pointer;"></i>  
-               </span>                                                   
+                   <img src="${result.info.secure_url}" class="img-thumbnail img-bordered" style="width: 220px; ml-2" delete="${result.info.delete_token}">
+                   <i class="fas fa-times btnDeleteImg" style="position: absolute;right: 0;top: 0; cursor: pointer;"></i>
+               </span>
                `;
             }
         }
@@ -46,7 +46,7 @@ $(document).ready(function () {
         Swal.fire({
             title: "Are you sure?",
             text: "You won't be able to revert this!",
-            icon: "warning", 
+            icon: "warning",
             showCancelButton: true,
             confirmButtonColor: "#3085d6",
             cancelButtonColor: "#d33",
@@ -131,6 +131,8 @@ $(document).ready(function () {
         var saleOff = $('input[name="saleOff"]').val();
         var description = $('textarea[name="description"]').val();
         var detail = editorData;
+        // alert(thumbnail);
+        // console.log(detail);
         var data = {
             name: name,
             brandID: brandID,
